e365033e66e39caf41de7ca315105ed62db792f6
ross-rl <ross@runloop.ai>
2024-01-30 14:41:20 -0800

    cp

--- the_wheel.py
+++ the_wheel.py
@@ -1,7 +1,8 @@
 import openai
 import runloop
-from openai.types.chat import ChatCompletionUserMessageParam, ChatCompletionSystemMessageParam
-
+from openai.types.chat import ChatCompletionUserMessageParam, ChatCompletionSystemMessageParam, ChatCompletionAssistantMessageParam, ChatCompletionMessageParam
+import json
+import pydantic
 
 @runloop.loop
 def wheel_it(metadata: dict[str, str], input: list[str]) -> tuple[list[str], dict[str, str]]:
@@ -18,18 +19,25 @@ _client = openai.OpenAI()
 _SYSTEM_MSG = ChatCompletionSystemMessageParam(content="You are a degenerate bookie with acute math skills", role="system")
 
 
+class MessageList(pydantic.BaseModel):
+    messages: list[ChatCompletionMessageParam]
+
+
 @runloop.loop
 def open_ai_data(metadata: dict[str, str], input: list[str]) -> tuple[list[str], dict[str, str]]:
     print(metadata)
     user_message = ChatCompletionUserMessageParam(content=input[0], role="user")
 
-    history = metadata.get("history", [])
+    history = json.loads(metadata.get("history", []))
 
     response = _client.chat.completions.create(
         model=_model,
         messages=[_SYSTEM_MSG] + history + [user_message],
     )
+    response.choices[0].message.model_dump_json()
 
-    metadata["history"] = history + [user_message] + [response.choices[0].message]
+    metadata["history"] = json.dumps(history + [user_message] +
+                                     [ChatCompletionAssistantMessageParam(content=response.choices[0].message.content,
+                                                                          role="assistant")])
 
     return [response.choices[0].message.content], metadata
