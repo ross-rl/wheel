a767117b34f5365df9675cfb4f14cab357f947ee
ross-rl <ross@runloop.ai>
2024-01-30 13:26:34 -0800

    cp

--- the_wheel.py
+++ the_wheel.py
@@ -2,5 +2,5 @@ import runloop
 
 
 @runloop.loop
-def wheel_it():
-    return "I am the wheel! let's start simple!"
+def wheel_it(metadata: dict[str, str], input: list[str]) -> tuple[list[str], dict[str, str]]:
+    return [f"I am the wheel! let's start simple! {input}"], metadata
