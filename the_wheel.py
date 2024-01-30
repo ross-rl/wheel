4ec397aa3f24a75c09c0130fd11e5e114f2f7ade
ross-rl <ross@runloop.ai>
2024-01-30 13:52:48 -0800

    cp

--- the_wheel.py
+++ the_wheel.py
@@ -1,4 +1,5 @@
 import runloop
+import openai
 
 
 @runloop.loop
@@ -10,3 +11,20 @@ def wheel_it(metadata: dict[str, str], input: list[str]) -> tuple[list[str], dic
 def sports_data(metadata: dict[str, str], input: list[str]) -> tuple[list[str], dict[str, str]]:
     return [f"The game was close, they had many points"], metadata
 
+
+_model = "gpt-4-1106-preview"
+_client = openai.OpenAI()
+_SYSTEM_MSG = {"role": "system",
+               "content": "You are a degenerate bookie"}
+
+
+@runloop.loop
+def open_ai_data(metadata: dict[str, str], input: list[str]) -> tuple[list[str], dict[str, str]]:
+    user_message = {"role": "user", "content": f"{input[0]}"}
+
+    response = _client.chat.completions.create(
+        model=_model,
+        messages=[_SYSTEM_MSG] + [user_message],
+    )
+
+    return [response.choices[0].message.content], metadata
