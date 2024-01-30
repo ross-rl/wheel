70ec8937a0a02e5ec6f96e5514f38fbc90f7abf4
ross-rl <ross@runloop.ai>
2024-01-30 13:44:20 -0800

    second endpoint

--- the_wheel.py
+++ the_wheel.py
@@ -4,3 +4,9 @@ import runloop
 @runloop.loop
 def wheel_it(metadata: dict[str, str], input: list[str]) -> tuple[list[str], dict[str, str]]:
     return [f"I am the wheel! let's start simple! {input}"], metadata
+
+
+@runloop.loop
+def sports_data(metadata: dict[str, str], input: list[str]) -> tuple[list[str], dict[str, str]]:
+    return [f"The game was close, they had many points"], metadata
+
